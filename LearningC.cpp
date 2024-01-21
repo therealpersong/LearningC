be94791d827bb6f84cd8f890b462421888303693
therealpersong <timotheussoares5@gmail.com>
2024-01-21 18:57:58 +0000

    some calculations and cleanup

--- LearningC.cpp
+++ LearningC.cpp
@@ -1,9 +1,12 @@
 #include <iostream> 
 int main()
 {
-
-// wow this is some nice comment
   int a = 11;
+  int b = 21;
+  int y = (a * b);
+
+  std::cout << y << '\n';
+  
   std::cout << "Hello world" << '\n';
   return 0;
 }
