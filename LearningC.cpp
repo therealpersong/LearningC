dbb02b145062536aa825517a5191d5110da3436f
therealpersong <timotheussoares5@gmail.com>
2024-01-28 09:39:54 +0000

    using structs

--- LearningC.cpp
+++ LearningC.cpp
@@ -1,14 +1,38 @@
 #include <iostream>
 
-template <typename T, typename U>
+struct student {
+    std::string name;
+    double gpa;
+    bool enrolled;
+};
 
-auto max(T x, U y){
-    return (x > y) ? x : y;
-}
+int main() {
+    student student1;
+    student1.name = "Spongebob";
+    student1.gpa = 3.2;
+    student1.enrolled = true;
 
-int main(){
+    std::cout << "The student's name is: " << student1.name << '\n';
+    std::cout << "The student's GPA is: " << student1.gpa << '\n';
+    std::cout << "Is the student enrolled?: " << student1.enrolled << '\n';
 
-    std::cout << max(1,2.1) << '\n';
+    student student2;
+    student2.name = "Patrick";  // Fix: Use student2 instead of student1
+    student2.gpa = 2.1;
+    student2.enrolled = true;
+
+    std::cout << "The student's name is: " << student2.name << '\n';
+    std::cout << "The student's GPA is: " << student2.gpa << '\n';
+    std::cout << "Is the student enrolled?: " << student2.enrolled << '\n';
+
+    student student3;
+    student3.name = "Squidward";  // Fix: Use student3 instead of student1
+    student3.gpa = 1.5;
+    student3.enrolled = false;
+
+    std::cout << "The student's name is: " << student3.name << '\n';
+    std::cout << "The student's GPA is: " << student3.gpa << '\n';
+    std::cout << "Is the student enrolled?: " << student3.enrolled << '\n';
 
     return 0;
 }
--- tempCodeRunnerFile.cpp
+++ tempCodeRunnerFile.cpp
@@ -1,14 +1,41 @@
-#include <iostream>
+#include <Iostream>
 
-template <typename T>
-
-T max(T x, T y){
-    return (x > y) ? x : y;
-}
+struct student{
+    std::string name;
+    double gpa;
+    bool enrolled;
+};
 
 int main(){
 
-    std::cout << max("1",2) << '\n';
+    student student1;
+    student1.name = "Spongebob";
+    student1.gpa = 3.2;
+    student1.enrolled = true;
+
+
+    std::cout << "The student's name is: " << student1.name << '\n';
+    std::cout << "The student's GPA is: " <<student1.gpa << '\n';
+    std::cout << "Is the student enrolled?: " << student1.enrolled << '\n';
+
+    student student2;
+    student1.name = "Patrick";
+    student1.gpa = 2.1;
+    student1.enrolled = true;
+
+    std::cout << "The student's name is: " << student2.name << '\n';
+    std::cout << "The student's GPA is: " <<student2.gpa << '\n';
+    std::cout << "Is the student enrolled?: " << student2.enrolled << '\n';
+
+    student student3;
+    student1.name = "Squidward";
+    student1.gpa = 1.5;
+    student1.enrolled = false;
+
+    std::cout << "The student's name is: " << student3.name << '\n';
+    std::cout << "The student's GPA is: " <<student3.gpa << '\n';
+    std::cout << "Is the student enrolled?: " << student3.enrolled << '\n';
+    
 
     return 0;
 }
