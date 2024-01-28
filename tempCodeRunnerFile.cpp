8d7bbd2ea91b80553ce985199ecb591e06080be1
therealpersong <timotheussoares5@gmail.com>
2024-01-28 08:49:57 +0000

    learning tic tac toe game

--- tempCodeRunnerFile.cpp
+++ tempCodeRunnerFile.cpp
@@ -1,45 +1,10 @@
 #include <iostream>
-#include <ctime>
-
-void drawBoard(char *spaces);
-void PlayerMove(char *spaces, char player);
-void ComputerMove(char *spaces, char computer);
-bool checkWinner(char *spaces, char player, char computer);
-bool checkTie(char *spaces);
-
 int main()
 {
-
-    char spaces[9] = {' ',' ',' ',' ',' ',' ',' ',' ',' '};
-    char player = 'X';
-    char computer = 'O';
-    bool running = true;
-
-    drawBoard(spaces);
-    return 0;
-}
-void drawBoard(char *spaces){
-    std::cout << '\n';
-    std::cout << "     |     |     " << '\n';
-    std::cout << "  " << spaces[0] <<"  |  " << spaces[1] <<"  |   " << spaces[2] <<"  " << '\n';
-    std::cout << "_____|_____|_____" << '\n';
-    std::cout << "     |     |     " << '\n';
-    std::cout << "  " << spaces[3] <<"  |  " << spaces[4] <<"  |   " << spaces[5] <<"  " << '\n';
-    std::cout << "_____|_____|_____" << '\n';
-    std::cout << "     |     |     " << '\n';
-    std::cout << "  " << spaces[6] <<"  |  " << spaces[7] <<"  |   " << spaces[8] <<"  " << '\n';
-    std::cout << "     |     |     " << '\n';
-    std::cout << '\n';
-}
-void PlayerMove(char *spaces, char player){
-
-}
-void ComputerMove(char *spaces, char computer){
-
-}
-bool checkWinner(char *spaces, char player, char computer){
-    return 0;
-}
-bool checkTie(char *spaces){
-    return 0;
+std::cout << "Please enter a character, an integer, and a double: ";
+char c = 0;
+int x = 0;
+double d = 0.0;
+std::cin >> c >> x >> d;
+std::cout << "You entered: " << c << ", " << x << " and " << d;
 }
