602f73e5035d2caaf9b15db0c5b35f3fe8b609bb
therealpersong <timotheussoares5@gmail.com>
2024-01-28 09:02:46 +0000

    Dynamic Memory

--- tempCodeRunnerFile.cpp
+++ tempCodeRunnerFile.cpp
@@ -1,10 +1,25 @@
 #include <iostream>
-int main()
-{
-std::cout << "Please enter a character, an integer, and a double: ";
-char c = 0;
-int x = 0;
-double d = 0.0;
-std::cin >> c >> x >> d;
-std::cout << "You entered: " << c << ", " << x << " and " << d;
+
+int main(){
+
+    char *pGrades = NULL;
+    int size;
+
+    std::cout << "How many grades to enter in?: ";
+    std::cin >> size;
+
+    pGrades = new char[5];
+
+    for(int i = 0; i < size; i++){
+        std::cout << "Enter grade #" << i + 1 << ": ";
+        std::cin >> pGrades[i];
+    }
+
+    for(int i = 0; i < size; i++){
+        std::cout << pGrades[i] << " ";
+    }
+
+    delete[] pGrades;
+    
+    return 0;
 }
