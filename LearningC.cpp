f2d375a3783fede9a1a31e2ffbfa17feb96360a4
therealpersong <timotheussoares5@gmail.com>
2024-01-29 06:15:32 +0000

    Learning getters and setters

--- LearningC.cpp
+++ LearningC.cpp
@@ -1,41 +1,38 @@
 #include <iostream>
 
-// Define a struct named Car
-struct Car {
-    std::string model;
-    int year;
-    std::string color;
+class Stove{
+    private:
+        int temperature = 0;
+    public:
+
+    int getTemperature(){
+        return temperature;
+    }
+    void setTemperature(int temperature){
+        if(temperature < 0){
+            this->temperature = 0;
+        }
+        else if(temperature >= 10){
+            this->temperature = temperature = 10;
+        }
+        else{
+            this->temperature = temperature;
+        }
+        
+    }
+
+
 };
 
-// Declare the function printCar that takes a Car parameter and prints its details
-void printCar(Car car);
+int main(){
+    Stove stove;
 
-int main() {
-    // Declare two Car objects named car1 and car2
-    Car car1;
-    Car car2;
+    //stove.temperature = 10000000000;
 
-    // Initialize car1 with specific values
-    car1.model = "Mustang";
-    car1.year = 2023;
-    car1.color = "red";
-
-    // Initialize car2 with specific values
-    car2.model = "Corvette";
-    car2.year = 2024;
-    car2.color = "blue";
-
-    // Call the printCar function to print details of car1
-    std::cout << &car1 << '\n';
-    printCar(car1);
-
-    return 0;
+    stove.setTemperature(0.5);
+    
+    std::cout << "The temperature setting is: " << stove.getTemperature();
 }
-
-// Define the printCar function to print details of a Car
-void printCar(Car car) {
-    std::cout << "The Address is: " << &car << '\n';
-    std::cout << "Model: " << car.model << '\n';  // Print the model
-    std::cout << "Year: " << car.year << '\n';    // Print the year
-    std::cout << "Color: " << car.color << '\n';  // Print the color
+void fege(){
+    //how to add a cool command
 }
