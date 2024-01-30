352f2f3813b45bb60f939bf76a0554a7a4c14cd8
therealpersong <timotheussoares5@gmail.com>
2024-01-30 06:40:35 +0000

    Learning with Classes

--- tempCodeRunnerFile.cpp
+++ tempCodeRunnerFile.cpp
@@ -1,23 +1,39 @@
-#include <Iostream>
+#include <iostream>
 
-class Animal{
+class Shape{
     public:
-    bool alive = true;
-    void eat(){
-        std::cout << "This animal is eating\n";
+    double area;
+    double volume;
+
+};
+
+class Cube : public Shape{
+    public:
+        double  side;
+    Cube(double side){
+        this->side = side;
+        this->area = side * side * 6;
+        this->volume = side * side * side;
     }
 };
 
-class Dog : public Animal{
-
+class Sphere : public Shape{
+    public:
+        double  radius;
+    Sphere(double radius){
+        this->radius = radius;
+        this->area = 4 * 3.14159 * (radius * radius);
+        this->volume = (4/3.0) * 3.1459 * (radius * radius * radius);
+    }
 };
 
 int main(){
 
-    Dog dog;
+    Cube cube(10);
+    Sphere sphere(5);
     
-    std::cout << dog.alive << '\n';
-    dog.eat();
+    std::cout << "Area: " << cube.area << "cm\n";
+    std::cout << "Volume: " << cube.volume << "cm\n";
 
     return 0;
 }
