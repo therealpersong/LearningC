f2602140727bf8dfb96b82550603db1b68c4ae43
therealpersong <timotheussoares5@gmail.com>
2024-01-31 17:27:47 +0000

    recapping arrays

--- LearningC.cpp
+++ LearningC.cpp
@@ -1,39 +1,15 @@
 #include <iostream>
+#include <ctime>
+#include <cmath>
 
-class Shape{
-    public:
-    double area;
-    double volume;
+int main() {
+    double ar[5] = {3.69, 6.69, 9.99, 12.55, 14.55};
 
-};
-
-class Cube : public Shape{
-    public:
-        double  side;
-    Cube(double side){
-        this->side = side;
-        this->area = side * side * 6;
-        this->volume = side * side * side;
+    for (int i = 0; i < 5; i++) {
+        std::cout << "Element at index " << i << ": " << ar[i] << std::endl;
     }
-};
 
-class Sphere : public Shape{
-    public:
-        double  radius;
-    Sphere(double radius){
-        this->radius = radius;
-        this->area = 4 * 3.14159 * (radius * radius);
-        this->volume = (4/3.0) * 3.1459 * (radius * radius * radius);
-    }
-};
-
-int main(){
-
-    Cube cube(10);
-    Sphere sphere(5);
-    
-    std::cout << "Area: " << cube.area << "cm\n";
-    std::cout << "Volume: " << cube.volume << "cm\n";
+    std::cout << "Code works perfectly" << std::endl;
 
     return 0;
 }
